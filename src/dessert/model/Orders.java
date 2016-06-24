7eebbd1d8f93a584284e06f914864f94f0492b03
tset <tset@192.168.1.110>
2016-06-24 10:48:39 +0800

    detail

--- src/dessert/model/Orders.java
+++ src/dessert/model/Orders.java
@@ -1,6 +1,7 @@
 package dessert.model;
 
 import java.io.Serializable;
+import java.util.function.Predicate;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -15,23 +16,70 @@ public class Orders implements Serializable{
 	private double totalPrice;//总额
 	private int  orderState;//状态
 	private  String orderDate;//日期
+	private String sendAddress;//送货地址
+	private String sendTime;//送货施加
+	private String sendWay;//配送方式
+	private String payWay;//付款方式
+	
 
 	public Orders(){
 		
 	}
 
-	public Orders(int orderId, int memberId, double totalPrice, int orderState, String orderDate) {
+	
+	public Orders(int orderId, int memberId, double totalPrice, int orderState, String orderDate, String sendAddress,
+			String sendTime, String sendWay, String payWay) {
 	
 		this.orderId = orderId;
 		this.memberId = memberId;
 		this.totalPrice = totalPrice;
 		this.orderState = orderState;
 		this.orderDate = orderDate;
+		this.sendAddress = sendAddress;
+		this.sendTime = sendTime;
+		this.sendWay = sendWay;
+		this.payWay = payWay;
 	}
 
 
+	public String getSendAddress() {
+		return sendAddress;
+	}
 
 
+	public void setSendAddress(String sendAddress) {
+		this.sendAddress = sendAddress;
+	}
+
+
+	public String getSendTime() {
+		return sendTime;
+	}
+
+
+	public void setSendTime(String sendTime) {
+		this.sendTime = sendTime;
+	}
+
+
+	public String getSendWay() {
+		return sendWay;
+	}
+
+
+	public void setSendWay(String sendWay) {
+		this.sendWay = sendWay;
+	}
+
+
+	public String getPayWay() {
+		return payWay;
+	}
+
+
+	public void setPayWay(String payWay) {
+		this.payWay = payWay;
+	}
 
 
 	public String getOrderDate() {
